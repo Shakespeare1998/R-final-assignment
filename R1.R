f478ece5778045c5b59c67dea686fb80e9e95440
Shakespeare1998 <athindra.bandi@gmail.com>
2021-07-02 22:14:18 +0530

    First part of the Assignment

---
+++ R1.R
@@ -0,0 +1,26 @@
+best<- function(state, oc){
+  outcome_short<- outcome[c("Hospital.Name", "State","Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack", "Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure", "Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia")]
+  outcome_short <- outcome_short[complete.cases(outcome_short),]
+  outcome_short <- transform(outcome_short, Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure = as.numeric(Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure),
+            Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia = as.numeric(Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia))
+  state_subset <- subset(outcome_short, State == state)
+
+  if(oc== "heart attack"){
+    m<-min(state_subset[3])
+    fin<- subset(state_subset, Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack<=m)
+  }
+  else if(oc == "heart failure"){
+    m<-min(state_subset[4])
+    fin<- subset(state_subset, Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure<=m)
+  }
+  else if(oc == "pneumonia"){
+    m<-min(state_subset[5])
+    fin<- subset(state_subset, Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia<=m)
+  }
+  else{
+    print("wrong choice")
+  }
+  
+  print(fin[["Hospital.Name"]])
+  
+}
