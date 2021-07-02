6dd326150cc6864eca04f42801cb2d24b1dd1d79
Shakespeare1998 <athindra.bandi@gmail.com>
2021-07-02 23:05:24 +0530

    Minor updates on part 1

--- R1.R
+++ R1.R
@@ -1,19 +1,22 @@
 best<- function(state, oc){
   outcome_short<- outcome[c("Hospital.Name", "State","Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack", "Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure", "Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia")]
-  outcome_short <- outcome_short[complete.cases(outcome_short),]
+  #outcome_short <- outcome_short[complete.cases(outcome_short),]
   outcome_short <- transform(outcome_short, Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure = as.numeric(Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure),
             Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia = as.numeric(Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia))
   state_subset <- subset(outcome_short, State == state)
 
   if(oc== "heart attack"){
+    state_subset <- state_subset[!is.na(state_subset$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack),]
     m<-min(state_subset[3])
     fin<- subset(state_subset, Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack<=m)
   }
   else if(oc == "heart failure"){
+    state_subset <- state_subset[!is.na(state_subset$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure),]
     m<-min(state_subset[4])
     fin<- subset(state_subset, Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure<=m)
   }
   else if(oc == "pneumonia"){
+    state_subset <- state_subset[!is.na(state_subset$Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia),]
     m<-min(state_subset[5])
     fin<- subset(state_subset, Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia<=m)
   }
